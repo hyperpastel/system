01c9092d6c5f4e443a3d78fe5dd9cfd3d2140fe2
hyperpastel <vmutze04@gmail.com>
2025-08-21 02:09:51 +0200

    feat(bluetooth): add bluetooth support

---
+++ modules/bluetooth.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+
+  services.blueman = {
+    enable = true;
+  };
+}
